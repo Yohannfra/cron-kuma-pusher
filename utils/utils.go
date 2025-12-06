b6fb362cc00d86ccf540f5403e0918076bf4901e
Yohann Assouline <yohannassouline@hotmail.fr>
2025-12-06 22:58:40 +0100

    chore: remove unused function EncodeParam

--- utils/utils.go
+++ utils/utils.go
@@ -1,13 +1,5 @@
 package utils
 
-import (
-	"net/url"
-)
-
-func EncodeParam(s string) string {
-	return url.QueryEscape(s)
-}
-
 func Ternary(cond bool, a, b string) string {
 	if cond {
 		return a
